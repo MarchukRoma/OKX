fd32e8ff1bfe29242deebf1dde7096c6bb5f843c
Roman Marchuk <marchukroman0036@gmail.com>
2024-09-30 23:18:12 +0300

    first commit

---
+++ src/components/Steps.jsx
@@ -0,0 +1,9 @@
+const Steps = () => {
+  return (
+    <section className="flex items-center justify-center">
+      <h1 className="text-[40px]">Qanday qilib USDT sotib olish mumkin</h1>
+    </section>
+  );
+};
+
+export default Steps;
